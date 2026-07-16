5a6e2131403f14b00d817a5d3314de98f110031f
neharanistudio <nehach7867@gmail.com>
2026-07-16 11:01:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,26 @@
-/* ===========================
- Neha Rani Studio JS
-=========================== */
-
-
-/* Sidebar Menu */
-
 const menuIcon = document.querySelector(".menu-icon");
 const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".menu-overlay");
 
 
-if(menuIcon && sidebar){
+if(menuIcon){
 
 menuIcon.addEventListener("click",()=>{
 
 sidebar.classList.toggle("active");
+overlay.classList.toggle("active");
 
 });
 
 }
 
 
+if(overlay){
 
-/* Wishlist */
+overlay.addEventListener("click",()=>{
 
-const wishlist = document.querySelectorAll(".wishlist");
-
-
-wishlist.forEach(item=>{
-
-item.addEventListener("click",()=>{
-
-item.classList.toggle("fa-solid");
-
-item.classList.toggle("fa-regular");
-
-});
-
-});
-
-
-
-/* Add To Cart */
-
-const cartButtons = document.querySelectorAll(".product-card button");
-
-
-cartButtons.forEach(button=>{
-
-button.addEventListener("click",()=>{
-
-
-button.innerHTML="Added ✓";
-
-
-button.style.background="#25D366";
-
-
-setTimeout(()=>{
-
-button.innerHTML="Add To Cart";
-
-button.style.background="#222";
-
-
-},2000);
-
-
-
-});
-
-
-});
-
-
-
-/* Back To Top */
-
-
-const topBtn=document.getElementById("topBtn");
-
-
-window.addEventListener("scroll",()=>{
-
-
-if(window.scrollY>400){
-
-topBtn.style.display="block";
-
-}
-
-else{
-
-topBtn.style.display="none";
-
-}
-
-
-});
-
-
-
-if(topBtn){
-
-topBtn.addEventListener("click",()=>{
-
-
-window.scrollTo({
-
-top:0,
-
-behavior:"smooth"
-
-});
-
+sidebar.classList.remove("active");
+overlay.classList.remove("active");
 
 });
 
