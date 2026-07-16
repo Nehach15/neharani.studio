c72444c15b1f64b3683aa8b478ee13486cc0d6dc
neharanistudio <nehach7867@gmail.com>
2026-07-16 11:07:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ overlay.classList.remove("active");
 });
 
 }
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".menu-overlay");
+
+
+if(menuBtn && sidebar && overlay){
+
+    menuBtn.addEventListener("click", function(){
+
+        sidebar.classList.add("active");
+        overlay.classList.add("active");
+
+    });
+
+
+    overlay.addEventListener("click", function(){
+
+        sidebar.classList.remove("active");
+        overlay.classList.remove("active");
+
+    });
+
+}
